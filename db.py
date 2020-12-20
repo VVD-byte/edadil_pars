9f6df7cfdbe9dc7b9b9adcfb290d6e7201cf75c2
VVD-byte <vovavoronin1999@gmail.com>
2020-12-21 01:45:07 +0300

    db add

--- db.py
+++ db.py
@@ -7,25 +7,91 @@ from datetime import datetime
 class DB:
     def __init__(self):
         self.get_city = 'SELECT * FROM discount_city;'
+        self.get_update_discounts = 'SELECT update_discounts FROM discount_company where name=%s'
+        self.get_len_discount_ = 'SELECT count(*) FROM discount where company_id=%s'
 
         self.city = self.get_pars_city()
 
-        self.add_data_discount = 'INSERT discount(created_date, last_modified_date, adult, company_id, description, original_company_id, original_id, original_url, product_name, removed, rubric1, rubric2, rubric3, thumbnail_url, update_elastic) VALUES (%s)'
-        self.add_data_discount_category = 'INSERT discount_category(name, subcategory_of, company_id) VALUES ()'
-        self.add_data_discount_city_company = 'INSERT discount_city_company(created_date, last_modified_date, city_id, update_discounts, company_id, update_filials, discount_count) VALUES ()'
+        self.add_data_discount_ = "INSERT discount (created_date, last_modified_date, adult, company_id, description, original_company_id, original_id, original_url, product_name, rubric1, rubric2, rubric3, thumbnail_url, update_elastic) VALUES('%s', '%s', %s, %s, '%s', %s, %s, '%s', '%s', '%s', '%s', '%s', '%s', %s);"
+        self.add_data_discount_category = "INSERT discount_category(name, subcategory_of, company_id) VALUES ()"
+        self.add_data_discount_city_company_ = 'INSERT discount_city_company(created_date, last_modified_date, city_id, update_discounts, company_id, update_filials, discount_count) VALUES ()'
         self.add_data_discount_city_company_price = 'INSERT discount_city_company_price(created_date, last_modified_date, abult, company_id, date_end, date_start, discount_id, old_price_note, original_company_id, price, promo_sort_hint, promo_str, promo_type, update_elastic, value_note) VALUES ()'
-        self.add_data_discount_company = 'INSERT discount_company(created_date, last_modified_date, city_id, discount_count, name, original_city_id, original_logo_url, upd_time, update_discounts, update_filials) VALUES ()'
+        self.add_data_discount_company_ = "INSERT discount_company(id, created_date, last_modified_date, discount_count, name, original_logo_url, upd_time, update_discounts, update_filials) VALUES (%s, '%s', '%s', %s, '%s', '%s', '%s', %s, %s)"
         self.add_data_discount_discount_category = 'INSERT discount_discount_category(discount_id, category_id) VALUES ()'
 
-
-    def add_data(self, dat, *args, **kwargs):
-        with closing(pymysql.connect(host='localhost',user='root',password='toor', db='pars', charset='utf8mb4', cursorclass=DictCursor)) as connection:
+    ####        add_new
+    def add_data_discount(self, dat, *args, **kwargs):
+        with closing(pymysql.connect(host='localhost',user='root',password='toor', db='pars', use_unicode = True, charset='utf8')) as connection:
             with connection.cursor() as cursor:
-                create_date = datetime.now()
+                create_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                modificate_date = create_date
+                adult = dat.get('adult', b'0')
+                company_id = kwargs.get('company_id', 16399)
+                description = dat.get('description', '').replace('  ', '').replace("'", '"')
+                original_company_id = company_id
+                original_id = dat.get('original_id')
+                original_url = dat.get('original_url')
+                product_name = dat.get('product_name').replace("'", '"')
+                rubric1 = kwargs.get('rubric1', None)
+                rubric2 = kwargs.get('rubric2', None)
+                rubric3 = kwargs.get('rubric3', None)
+                thumbnail_url = dat.get('thumbnail_url')
+                update_elastic = b'0'
+                try:
+                    cursor.execute(self.add_data_discount_ %(create_date, modificate_date, adult, company_id, description, original_company_id, original_id, original_url, product_name, rubric1, rubric2, rubric3, thumbnail_url, update_elastic))
+                except Error as e:
+                    print(self.add_data_discount_ %(create_date, modificate_date, adult, company_id, description, original_company_id, original_id, original_url, product_name, rubric1, rubric2, rubric3, thumbnail_url, update_elastic))
+                    raise e
+                connection.commit()
 
+    def add_data_discount_company(self, dat, *args, **kwargs):
+        with closing(pymysql.connect(host='localhost', user='root', password='toor', db='pars', use_unicode=True, charset='utf8')) as connection:
+            with connection.cursor() as cursor:
+                id = dat.get('id', 16399)
+                create_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                last_modified_date = create_date
+                name = dat.get('name', 'xxx')
+                original_logo_url = dat.get('original_logo_url', '')
+                upd_time = create_date
+                update_discounts = b'1'
+                update_filials = b'1'
+                dicount_count = dat.get('discount_count', 0)
+                try:
+                    cursor.execute(self.add_data_discount_company_ %(id, create_date, last_modified_date, dicount_count, name, original_logo_url, upd_time, update_discounts, update_filials))
+                except Error as e:
+                    print(self.add_data_discount_company_ %(id, create_date, last_modified_date, dicount_count, name, original_logo_url, upd_time, update_discounts, update_filials))
+                    raise e
+                connection.commit()
 
+    ####        update_data
+
+    ####        get_data
     def get_pars_city(self):
         with closing(pymysql.connect(host='localhost',user='root',password='toor', db='pars', charset='utf8mb4', cursorclass=DictCursor)) as connection:
             with connection.cursor() as cursor:
                 cursor.execute(self.get_city)
                 return [i['name'] for i in cursor.fetchall() if i['update_discounts'] == b'\x01']
+
+    def get_len_discount(self, id):
+        with closing(pymysql.connect(host='localhost', user='root', password='toor', db='pars', use_unicode=True, charset='utf8')) as connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(self.get_len_discount_ %id)
+                    return cursor.fetchall()[0][0]
+                except: return 0
+
+    def get_update_discounts_discount_company(self, name, *args, **kwargs):
+        with closing(pymysql.connect(host='localhost', user='root', password='toor', db='pars', use_unicode=True, charset='utf8')) as connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(self.get_update_discounts %name)
+                    if cursor.fetchall()[0] == b'0': return False
+                    else: return True, True
+                except: return True, False #второе значение для определения существует ли сейчас данное поле, чтобы выбрать создавать его или обнолять
+
+    ####        manipulate_data
+    def clear_all_discount(self):
+        with closing(pymysql.connect(host='localhost',user='root',password='toor', db='pars', charset='utf8mb4', cursorclass=DictCursor)) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(f'TRUNCATE TABLE discount')
+                connection.commit()
--- parser_fixprice.py
+++ parser_fixprice.py
@@ -39,17 +39,22 @@ class pars_fixprice(pars):
         }
         self.url = 'https://fix-price.ru/catalog/'
         self.rub = {}
-        self.cit = 'Москва'
+        self.magasine_date = {'id':16400, 'name': 'fix_price', 'original_logo_url':'', 'discount_count': 0}
 
     def start_pars(self):
-        for i in self.city:
-            self.cit = i
-            logging.info(f'Выбран город {i}')
-            self.cookies['CURRENT_LOCATION_ID'] = self.city_id[i]
-            self.rubric()
-            logging.info(f'Собраны все рубрики города {i}')
-            self.pars_id_tovar()
-            break
+        if self.get_update_discounts_discount_company(self.magasine_date['name'])[0]:
+            self.magasine_date['original_logo_url'] = self.get_soup('https://fix-price.ru/catalog/').find_all('img', {'class':'card-img'})[0].get('src')
+            for i in self.city:
+                logging.info(f'Выбран город {i}')
+                self.cookies['CURRENT_LOCATION_ID'] = self.city_id[i]
+                self.rubric()
+                logging.info(f'Собраны все рубрики города {i}')
+                self.pars_id_tovar()
+                break                       ##########################################################
+            self.magasine_date['discount_count'] = self.get_len_discount(self.magasine_date['id'])
+            if not self.get_update_discounts_discount_company(self.magasine_date['name'])[1]:
+                self.add_data_discount_company(self.magasine_date)
+                logging.info(f'{self.magasine_date["name"]} DB discount_company ADD')
 
     #парсит все рубрики и заполняет dict
     def rubric(self):
@@ -61,21 +66,23 @@ class pars_fixprice(pars):
                     self.rub[i.find('a').text][j.text.replace('  ', '').replace('\n', '')]['src'] = 'https://fix-price.ru' + j.find('a').get('href')
                 if not self.rub[i.find('a').text]:
                     self.rub[i.find('a').text]['src'] = 'https://fix-price.ru' + i.find('a').get('href')
+                break           #######################################################
 
     #парсит id всех товаров данной рубрики
     def pars_page(self, url, data):
-        for i in range(1, self.get_max_page(url))[:1]:
+        for i in range(1, self.get_max_page(url))[:1]:  ############################################ [:1]
             soup = self.get_soup(url + '?PAGEN_1=%s' %i)
             all_price = soup.find_all('span', {'style': 'font-family: RotondaC-Bold;'})
             all_img = soup.find_all('img', {'itemprop':'image'})
             for idd, j in enumerate(soup.find_all('a', {'class':'product-card__title'})):
                 _id = int(j.get('href').split('/')[-2])
                 data[_id] = {}
-                data[_id]['src'] = 'https://fix-price.ru' + j.get('href')
-                data[_id]['img_src'] = all_img[idd].get('src')
-                data[_id]['name'] = j.text.replace('  ', '').replace('\r', '').replace('\n', '')
+                data[_id]['original_id'] = _id
+                data[_id]['original_url'] = 'https://fix-price.ru' + j.get('href')
+                data[_id]['thumbnail_url'] = all_img[idd].get('src')
+                data[_id]['product_name'] = j.text.replace('  ', '').replace('\r', '').replace('\n', '')
                 data[_id]['price'] = all_price[idd].text
-                data[_id]['years'] = ''
+                data[_id]['adult'] = b'0'
                 try:
                     data[_id]['price_copey'] = str(int(all_price[idd].text) * 60)
                 except: data[_id]['price_copey'] = int(float(all_price[idd].text.replace(',', '.'))//1 * 60 + float(all_price[idd].text.replace(',', '.'))%1 * 60)
@@ -83,24 +90,26 @@ class pars_fixprice(pars):
     def pars_data_product(self, data, *args, **kwargs):
         for i in data:
             if i != 'src':
-                soup = self.get_soup(data[i]['src'])
-                data[i]['opisanie'] = soup.find('div', {'class':'tab__text'})
-                self.add_data(data.pop(i), *args)
+                soup = self.get_soup(data[i]['original_url'])
+                data[i]['description'] = soup.find('div', {'class':'tab__text'}).text
+                logging.info(f'{self.magasine_date["name"]} DB discount ADD {i}')
+                self.add_data_discount(data[i], *args, **kwargs)
 
     #просматривает рубрики и отправляет на парсинг
     def pars_id_tovar(self):
+        self.clear_all_discount()                   ###########################################
         for i, items in self.rub.items():
             logging.info(f'Парсим "{i}"')
             if 'src' in items:
                 self.pars_page(items['src'], self.rub[i])
-                self.pars_data_product(self.rub[i], i)
+                self.pars_data_product(self.rub[i], rubric1 = i, company_id = self.magasine_date['id'])
+                self.rub[i] = {}
             else:
                 for j, item in items.items():
                     logging.info(f'Парсим {i} - {j}')
                     self.pars_page(item['src'], self.rub[i][j])
-                    self.pars_data_product(self.rub[i][j], i, j)
-        with open('prod.json', 'w') as t: t.write(json.dumps(self.rub).encode('cp1251').decode('unicode-escape'))
-        logging.info('ЗАПИСЬ ПРОШЛА УСПЕШНО')
+                    self.pars_data_product(self.rub[i][j], rubric1 = i, rubric2 = j, company_id = self.magasine_date['id'])
+                    self.rub[i][j] = {}
 
     #смотрит количество страниц рубрики
     def get_max_page(self, url):
